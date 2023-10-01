9de4edf29ac5e90884d000f2817569c2f159ba18
github akito-10 <akt.fukuda@gmail.com>
2023-10-02 01:44:25 +0900

    mock

--- src/db/memo.ts
+++ src/db/memo.ts
@@ -1,1 +1,18 @@
-export const MEMOS = [];
+import { Memo } from "src/schemas/memos/type";
+
+export const MEMOS: Memo[] = [
+  {
+    id: "1",
+    title: "Hello Elysia",
+    content: "Welcome to Elysia",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: "2",
+    title: "Hello Elysia",
+    content: "Welcome to Elysia",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
