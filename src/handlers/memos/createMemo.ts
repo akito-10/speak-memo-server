9d09be1a13168dac0eddb01a68e605d549dd56e2
github akito-10 <akt.fukuda@gmail.com>
2023-10-02 22:56:21 +0900

    createMemo Promise

--- src/handlers/memos/createMemo.ts
+++ src/handlers/memos/createMemo.ts
@@ -4,8 +4,8 @@ import { MEMOS } from "src/db/memo";
 import { CreateMemoDto } from "src/dto/createMemoDto";
 
 export const createMemo = {
-  handler: ({ body }: { body: CreateMemoDto }) => {
-    return db.memo.create({ data: body });
+  handler: async ({ body }: { body: CreateMemoDto }) => {
+    return await db.memo.create({ data: body });
   },
   hook: {
     body: t.Object({
